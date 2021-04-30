94028ca557b2058105d804fbb9e821918ff97fd4
Penguin-God <gkswh4860@naver.com>
2021-04-30 21:05:01 +0900

    파일 정리

--- Project/기념일 카운트/유니티시작일.js
+++ Project/기념일 카운트/유니티시작일.js
@@ -1,7 +1,8 @@
+// github.com/funnycom/js-basic-book
+
 const unityStartDay = new Date("2020-4-16");
 let today = new Date();
 
 let passedTime = today.getTime() - unityStartDay.getTime();
-let passedDay = Math.round(passedTime / (1000 * 60 * 60 * 24));
+let passedDay = Math.ceil(passedTime / (1000 * 60 * 60 * 24));
 
-// github.com/funnycom/js-basic-book
