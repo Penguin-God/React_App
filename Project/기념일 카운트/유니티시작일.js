d2b2792fd8d09e3413db5420bc98ce12365e3901
Penguin-God <gkswh4860@naver.com>
2021-05-03 18:17:16 +0900

    unity 기념일 세는 웹페이지

--- Project/기념일 카운트/유니티시작일.js
+++ Project/기념일 카운트/유니티시작일.js
@@ -3,6 +3,23 @@
 const unityStartDay = new Date("2020-4-16");
 let today = new Date();
 
-let passedTime = today.getTime() - unityStartDay.getTime();
+const unity_StartTime = unityStartDay.getTime();
+let passedTime = today.getTime() - unity_StartTime;
 let passedDay = Math.ceil(passedTime / (1000 * 60 * 60 * 24));
+document.querySelector("#accent").innerHTML = passedDay + "일";
+
+function GetFutureDay(passDay){
+    let futureTime = unityStartDay.getTime() + passDay * (1000 * 60 * 60 * 24);
+    const futureDate = new Date(futureTime);
+    
+    const futureYeer = futureDate.getFullYear();
+    const futureMonth = futureDate.getMonth();
+    const futureDay = futureDate.getDate();
+    let dayId = "#date" + passDay;
+    document.querySelector(dayId).innerHTML = futureYeer + "년 " +  futureMonth + "월 " + futureDay + "일";    
+}
+
+GetFutureDay(500);
+GetFutureDay(1000);
+GetFutureDay(2000);
 
